1e73e56dc8f1179f98caf0aeccf25c11f9c1b694
MICHAEL HOFFMAN <mphoffman8390@gmail.com>
2026-01-16 19:15:55 -0600

    added files client, index, and seed to db

---
+++ db/client.js
@@ -0,0 +1,15 @@
+const {Client} = require('pg')
+
+const DB_NAME = 'ACS-dev';
+
+const connectionString =
+    process.env.DATABASE_URL || `postgres:localhost:5432/${DB_NAME}`;
+
+    const client = new Client({
+        connectionString,
+        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+    });
+
+    client.connect()
+
+    module.exports = client;
