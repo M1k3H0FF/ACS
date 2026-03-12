6e789b21ba503a92a6601355302885b8ff9db7cd
MICHAEL HOFFMAN <mphoffman8390@gmail.com>
2026-03-12 18:31:54 -0500

    first commit

---
+++ data/todos.js
@@ -0,0 +1,19 @@
+export const data = [
+    {
+        "id":1,
+        "title": "Bank Left",
+        "completed": true
+    },
+    {
+        "id":2,
+        "title": "Bank Right",
+        "completed": true
+    },
+    {
+        "id":3,
+        "title": "Do a Barrel Roll",
+        "completed": true
+    }
+]
+
+
